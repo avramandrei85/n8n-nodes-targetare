535e477b27bdfcccb1721783b80b234529b1b4b0
Andrei Avram <aavram@makeitfuture.com>
2025-07-15 13:56:09 +0300

    Updated the test credentials part

--- credentials/TargetareApi.credentials.ts
+++ credentials/TargetareApi.credentials.ts
@@ -1,48 +1,48 @@
 import {
 	IAuthenticateGeneric,
-	ICredentialType,
 	ICredentialTestRequest,
+	ICredentialType,
 	INodeProperties,
-  } from 'n8n-workflow';
-  
-  export class TargetareApi implements ICredentialType {
+} from 'n8n-workflow';
+
+export class TargetareApi implements ICredentialType {
 	name = 'TargetareApi';
 	displayName = 'Targetare API';
+
 	documentationUrl = 'https://api.targetare.ro/auth/';
-	
+
 	properties: INodeProperties[] = [
-	  {
-		displayName: 'API Key',
-		name: 'apiKey',
-		type: 'string',
-		default: '',
-		typeOptions: {
-		  password: true,
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
 		},
-	  },
 	];
-  
-	authenticate = {
-	  type: 'generic',
-	  properties: {
-		headers: {
-		  Authorization: '={{ "Bearer " + $credentials.apiKey }}'
-		}
-	  },
-	} as IAuthenticateGeneric;
-  
-	test = {
-	  request: {
-		baseURL: 'https://api.targetare.ro/v1',
-		method: 'GET',
-		url: '/companies/',
-		qs: {
-			registration_date: '2020-01-01'
-		  },
-		headers: {
-		  Authorization: '={{"Bearer " + $credentials.apiKey}}'
-		}
-	  }
-	} as ICredentialTestRequest;
-  }
-  
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '={{ "Bearer " + $credentials.apiKey }}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			method: 'GET',
+			baseURL: 'https://api.targetare.ro/v1',
+			url: '/companies/',
+			qs: {
+				registration_date: '2020-01-01',
+			},
+			headers: {
+				Authorization: '={{ "Bearer " + $credentials.apiKey }}',
+			},
+		},
+	};
+}
